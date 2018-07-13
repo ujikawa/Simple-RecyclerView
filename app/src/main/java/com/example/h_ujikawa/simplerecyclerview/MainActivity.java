8e8e47b5a0e2c065697d1848cd1ad19a44106d6b
ujikawa <ujikawa1026@gmail.com>
2018-07-13 23:58:54 +0900

    Swipe&Drag&Drop

--- app/src/main/java/com/example/h_ujikawa/simplerecyclerview/MainActivity.java
+++ app/src/main/java/com/example/h_ujikawa/simplerecyclerview/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -17,6 +18,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerViewAdapter recyclerViewAdapter;
+    private List<String> dataset = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,27 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerViewAdapter = new RecyclerViewAdapter(getListData());
         binding.recyclerView.setAdapter(recyclerViewAdapter);
+
+        ItemTouchHelper mIth = new ItemTouchHelper(
+                new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN,
+                        ItemTouchHelper.RIGHT) {
+                    @Override
+                    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                        final int fromPos = viewHolder.getAdapterPosition();
+                        final int toPos = target.getAdapterPosition();
+                        recyclerViewAdapter.notifyItemMoved(fromPos, toPos);
+                        return true;// true if moved, false otherwise
+                    }
+
+                    @Override
+                    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                        final int fromPos = viewHolder.getAdapterPosition();
+                        dataset.remove(fromPos);
+                        recyclerViewAdapter.notifyItemRemoved(fromPos);
+                    }
+                }
+        );
+        mIth.attachToRecyclerView(binding.recyclerView);
     }
 
     private List<String> getListData() {
@@ -45,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
         list.add("神無月");
         list.add("霜月");
         list.add("師走");
+        dataset = list;
         return list;
     }
 
@@ -52,6 +76,29 @@ public class MainActivity extends AppCompatActivity {
 
         private List<String> dataSet;
 
+        RecyclerViewAdapter(List<String> myDataSet) {
+            dataSet = myDataSet;
+        }
+
+        @Override //ViewHolderを返す
+        public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            RowBinding binding = RowBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+            return new ViewHolder(binding);
+        }
+
+        @Override //ViewHolderを貰ってデータと紐付け
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            String text = dataSet.get(position);
+            holder.binding.rowTextView.setText(text);
+            holder.binding.executePendingBindings();
+        }
+
+        @Override //このカウント数分のonBindViewHolderが呼ばれる
+        public int getItemCount() {
+            return dataSet.size();
+        }
+
+        //Viewへの参照を持つ
         class ViewHolder extends RecyclerView.ViewHolder {
             final RowBinding binding;
 
@@ -59,28 +106,6 @@ public class MainActivity extends AppCompatActivity {
                 super(binding.getRoot());
                 this.binding = binding;
             }
-        }
-
-        RecyclerViewAdapter(List<String> myDataSet) {
-            dataSet = myDataSet;
-        }
-
-        @Override
-        public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            RowBinding binding = RowBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
-            return new ViewHolder(binding);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            String text = dataSet.get(position);
-            holder.binding.rowTextView.setText(text);
-            holder.binding.executePendingBindings();
-        }
-
-        @Override
-        public int getItemCount() {
-            return dataSet.size();
         }
 
     }
