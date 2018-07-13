20a24bf6e57e902491e150e6995717d11b98ae2a
ujikawa <ujikawa1026@gmail.com>
2018-07-13 23:12:19 +0900

    update

--- app/src/main/java/com/example/h_ujikawa/simplerecyclerview/MainActivity.java
+++ app/src/main/java/com/example/h_ujikawa/simplerecyclerview/MainActivity.java
@@ -4,8 +4,12 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 import com.example.h_ujikawa.simplerecyclerview.databinding.ActivityMainBinding;
+import com.example.h_ujikawa.simplerecyclerview.databinding.RowBinding;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,5 +46,42 @@ public class MainActivity extends AppCompatActivity {
         list.add("霜月");
         list.add("師走");
         return list;
+    }
+
+    class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+        private List<String> dataSet;
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            final RowBinding binding;
+
+            ViewHolder(RowBinding binding) {
+                super(binding.getRoot());
+                this.binding = binding;
+            }
+        }
+
+        RecyclerViewAdapter(List<String> myDataSet) {
+            dataSet = myDataSet;
+        }
+
+        @Override
+        public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            RowBinding binding = RowBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+            return new ViewHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            String text = dataSet.get(position);
+            holder.binding.rowTextView.setText(text);
+            holder.binding.executePendingBindings();
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataSet.size();
+        }
+
     }
 }
